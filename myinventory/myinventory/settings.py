4aa7215b4b215db6f451b0f81f5fd1f8c8339995
Gene Pan <pangene@gmail.com>
2021-03-22 01:56:32 -0700

    setup js build pipeline

--- myinventory/myinventory/settings.py
+++ myinventory/myinventory/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'myinventory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
---
+++ myinventory/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/index.js',
+  output: {
+    filename: 'index-bundle.js',
+    path: path.resolve(__dirname, './static'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env", 
+            "@babel/preset-react"
+          ]
+        }
+      }
+    ]
+  }
+};
