ef2416b842312b65246dc5f7b392ea4e701731f7
Gene Pan <pangene@gmail.com>
2021-04-03 00:52:11 -0700

    Added django-heroku

--- myinventory/settings.py
+++ myinventory/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +157,6 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # Taggit config
 
 TAGGIT_CASE_INSENSITIVE = True
+
+
+django_heroku.settings(locals())
